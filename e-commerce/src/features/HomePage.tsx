324dcc49110735ea148076b59f2c7a03accefd51
asifm.ashique <asifm.ashique@petronas.com>
2022-03-03 17:20:28 +0600

    rem and bem done

--- e-commerce/src/features/HomePage.tsx
+++ e-commerce/src/features/HomePage.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 
 function HomePage() {
   return (
+    /*
+    BEM -> Bolck Element Modifier
+    here  
+    1. header is a Bolck
+    2. header__logo-box so __logo-box is an element
+    3. heading-primary--main so --main is a modifier
+    */
     <div className='header'>
-      <div className='logo-box'>
-        <img src='../assets/logo-white.png' alt='Logo' className='logo' />
+      <div className='header__logo-box'>
+        <img
+          src='../assets/logo-white.png'
+          alt='Logo'
+          className='header__logo'
+        />
       </div>
-      <div className='text-box'>
+      <div className='header__text-box'>
         <h1 className='heading-primary'>
-          <span className='heading-primary-main'>E-commerce</span>
-          <span className='heading-primary-sub'>is where life happens</span>
+          <span className='heading-primary--main'>E-commerce</span>
+          <span className='heading-primary--sub'>is where life happens</span>
         </h1>
 
-        <a href='#' className='btn btn-white btn-animated'>
+        <a href='#' className='btn btn--white btn--animated'>
           Find our product
         </a>
       </div>
