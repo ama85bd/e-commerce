a5273e2fecc55e7df767b0aa08e01b8a5cd96b7d
asifm.ashique <asifm.ashique@petronas.com>
2022-03-04 12:47:22 +0600

    complete grid with float

--- e-commerce/src/features/HomePage.tsx
+++ e-commerce/src/features/HomePage.tsx
@@ -10,24 +10,56 @@ function HomePage() {
     2. header__logo-box so __logo-box is an element
     3. heading-primary--main so --main is a modifier
     */
-    <div className='header'>
-      <div className='header__logo-box'>
-        <img
-          src='../assets/logo-white.png'
-          alt='Logo'
-          className='header__logo'
-        />
-      </div>
-      <div className='header__text-box'>
-        <h1 className='heading-primary'>
-          <span className='heading-primary--main'>E-commerce</span>
-          <span className='heading-primary--sub'>is where life happens</span>
-        </h1>
+    <div>
+      <div className='header'>
+        <div className='header__logo-box'>
+          <img
+            src='../assets/logo-white.png'
+            alt='Logo'
+            className='header__logo'
+          />
+        </div>
+        <div className='header__text-box'>
+          <h1 className='heading-primary'>
+            <span className='heading-primary--main'>E-commerce</span>
+            <span className='heading-primary--sub'>is where life happens</span>
+          </h1>
 
-        <a href='#' className='btn btn--white btn--animated'>
-          Find our product
-        </a>
+          <a href='#' className='btn btn--white btn--animated'>
+            Find our product
+          </a>
+        </div>
       </div>
+      <section className='grid-test'>
+        <div className='row'>
+          <div className='col-1-of-2'>Col 1 of 2</div>
+          <div className='col-1-of-2'>Col 1 of 2</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+          <div className='col-1-of-3'>Col 1 of 3</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-3'> Col 1 of 3</div>
+          <div className='col-2-of-3'>Col 2 of 3</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-2-of-4'>Col 2 of 4</div>
+        </div>
+        <div className='row'>
+          <div className='col-1-of-4'>Col 1 of 4</div>
+          <div className='col-3-of-4'>Col 3 of 4</div>
+        </div>
+      </section>
     </div>
   );
 }
