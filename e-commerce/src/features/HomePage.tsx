1ed35e3b596e901fe2c010001b9aa8b6efbe1b67
asifm.ashique <asifm.ashique@petronas.com>
2022-03-06 19:34:44 +0600

    till form

--- e-commerce/src/features/HomePage.tsx
+++ e-commerce/src/features/HomePage.tsx
@@ -144,14 +144,214 @@ function HomePage() {
           <div className='row'>
             <div className='col-1-of-3'>
               <div className='card'>
-                <div className='card__side'>Card text</div>
+                <div className='card__side card__side--front'>
+                  <div className='card__picture card__picture--1'>&nbsp;</div>
+                  <h4 className='card__heading'>
+                    <span className='card__heading-span card__heading-span--1'>
+                      The sea explorer
+                    </span>
+                  </h4>
+                  <div className='card__details'>
+                    <ul>
+                      <li>3 day tours</li>
+                      <li>up to 30 people</li>
+                      <li>2 tour guides</li>
+                      <li>sleep in cozy hotels</li>
+                      <li>difficulty: easy</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='card__side card__side--back card__side--back-1'>
+                  {/*cta = call to action*/}
+                  <div className='card__cta'>
+                    <div className='card__price-box'>
+                      <p className='card__price-only'>Only</p>
+                      <p className='card__price-value'>$297</p>
+                    </div>
+                    <a href='#' className='btn btn--white'>
+                      Book now!
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
             <div className='col-1-of-3'>
-              <div className='card'>Card text</div>
+              <div className='card'>
+                <div className='card__side card__side--front'>
+                  <div className='card__picture card__picture--2'>&nbsp;</div>
+                  <h4 className='card__heading'>
+                    <span className='card__heading-span card__heading-span--2'>
+                      the forest hiker
+                    </span>
+                  </h4>
+                  <div className='card__details'>
+                    <ul>
+                      <li>7 day tours</li>
+                      <li>up to 40 people</li>
+                      <li>6 tour guides</li>
+                      <li>sleep in provided tents</li>
+                      <li>difficulty: medium</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='card__side card__side--back card__side--back-2'>
+                  {/*cta = call to action*/}
+                  <div className='card__cta'>
+                    <div className='card__price-box'>
+                      <p className='card__price-only'>Only</p>
+                      <p className='card__price-value'>$497</p>
+                    </div>
+                    <a href='#' className='btn btn--white'>
+                      Book now!
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className='col-1-of-3'>
-              <div className='card'>Card text</div>
+              <div className='card'>
+                <div className='card__side card__side--front'>
+                  <div className='card__picture card__picture--3'>&nbsp;</div>
+                  <h4 className='card__heading'>
+                    <span className='card__heading-span card__heading-span--3'>
+                      the show adventurer
+                    </span>
+                  </h4>
+                  <div className='card__details'>
+                    <ul>
+                      <li>5 day tours</li>
+                      <li>up to 15 people</li>
+                      <li>3 tour guides</li>
+                      <li>sleep in cozy hotels</li>
+                      <li>difficulty: hard</li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='card__side card__side--back card__side--back-3'>
+                  {/*cta = call to action*/}
+                  <div className='card__cta'>
+                    <div className='card__price-box'>
+                      <p className='card__price-only'>Only</p>
+                      <p className='card__price-value'>$897</p>
+                    </div>
+                    <a href='#' className='btn btn--white'>
+                      Book now!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='u-center-text u-margin-top-huge'>
+            <a href='#' className='btn btn--green'>
+              Discover all tours
+            </a>
+          </div>
+        </section>
+        <section className='section-stories'>
+          <div className='bg-video'>
+            <video className='bg-video__content' autoPlay muted loop>
+              <source src='../assets/video.mp4' type='video/mp4' />
+              <source src='../assets/video.webm' type='video/webm' />
+              Your browser is not supported!
+            </video>
+          </div>
+
+          <div className='u-center-text u-margin-bottom-big'>
+            <h2 className='heading-secondary'>
+              We make people genuinely happy
+            </h2>
+          </div>
+          <div className='row'>
+            <div className='story'>
+              <figure className='story__shape'>
+                <img
+                  src='../assets/nat-8.jpg'
+                  alt='Person on a tour'
+                  className='story__img'
+                />
+                <figcaption className='story__caption'>Mary Smith</figcaption>
+              </figure>
+              <div className='story__text'>
+                <h3 className='heading-tertiary u-margin-bottom-small'>
+                  i had the best week ever with my family
+                </h3>
+                <p className='feature-box__text'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                  nam. Dolor nam. Dolor nam.Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Dolor nam. Dolor nam. Dolor
+                  nam.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolor nam. Dolor nam. Dolor nam.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='story'>
+              <figure className='story__shape'>
+                <img
+                  src='../assets/nat-9.jpg'
+                  alt='Person on a tour'
+                  className='story__img'
+                />
+                <figcaption className='story__caption'>Jack Wilson</figcaption>
+              </figure>
+              <div className='story__text'>
+                <h3 className='heading-tertiary u-margin-bottom-small'>
+                  wow! my life is completely different now
+                </h3>
+                <p className='feature-box__text'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                  nam. Dolor nam. Dolor nam.Lorem ipsum dolor sit amet
+                  consectetur adipisicing elit. Dolor nam. Dolor nam. Dolor
+                  nam.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolor nam. Dolor nam. Dolor nam.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className='u-center-text u-margin-top-huge'>
+            <a href='#' className='btn-text'>
+              Read all stories&rarr;
+            </a>
+          </div>
+        </section>
+
+        <section className='section-book'>
+          <div className='row'>
+            <div className='book'>
+              <div className='book__form'>
+                <form action='#' className='form'>
+                  <div className=' u-margin-bottom-medium'>
+                    <h2 className='heading-secondary'>Start booking now</h2>
+                  </div>
+                  <div className='form__group'>
+                    <input
+                      type='text'
+                      className='form__input'
+                      placeholder='Full name'
+                      id='name'
+                      required
+                    />
+                    <label htmlFor='name' className='form__label'>
+                      Full name
+                    </label>
+                  </div>
+                  <div className='form__group'>
+                    <input
+                      type='email'
+                      className='form__input'
+                      placeholder='Email address'
+                      id='email'
+                      required
+                    />
+                    <label htmlFor='email' className='form__label'>
+                      Email address
+                    </label>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </section>
